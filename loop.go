0ab72f22d073d0ade689deda42236f20a95bffe8
Sun-139 <215622369+Sun-139@users.noreply.github.com>
2026-03-21 13:51:11 +0530

    loops with basic problems

---
+++ loop.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Printing numbers from 1 to 9 using loops
+	fmt.Println("Numbers from 1 to 9")
+	for i := 1; i <= 9; i++ {
+		fmt.Println(i)
+	}
+	fmt.Println("")
+
+	//Printing even numbers
+	fmt.Println("Even numbers")
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	fmt.Println("")
+
+	//Printing odd numbers
+	fmt.Println("Odd numbers")
+	for i := 1; i <= 10; i++ {
+		if i%2 != 0 {
+			fmt.Println(i)
+		}
+	}
+	fmt.Println("")
+
+	//Printing table of 9
+	fmt.Println("Table of 9")
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i, "* 9", "=", i*9)
+	}
+}
